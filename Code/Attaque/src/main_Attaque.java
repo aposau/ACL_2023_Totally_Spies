54ab1062a8a1d722e5b3a98287245033473cbcc1
romane Ransan <ransanrr@gmail.com>
2023-11-14 16:24:56 +0100

    src attaque + speciaux + objet speciaux

--- Code/Attaque/src/main_Attaque.java
+++ Code/Attaque/src/main_Attaque.java
@@ -1,28 +1,34 @@
 package proj_Ro;
 
+ 
 public class main_Attaque {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int pv_mon=20;
+		int pv_mon=150;
 		int pv_hero=150;
+		int plein=1;
 		Personnage monstre= new Personnage(pv_mon,10,3);
-		Personnage hero= new Personnage(pv_hero,10,3);
+		Personnage hero= new Personnage(pv_hero,20,10);
+		Objet_spe potion=new Objet_spe(plein,3,4);
+		Objet_spe trou=new Objet_spe(plein,15,4);
+		Objet_spe pic=new Objet_spe(plein,20,10);
 
-		
 		Attaque a=new Attaque(monstre, hero);
+		Special s=new Special(hero,potion,trou,pic);
 		
-		
-		System.out.println(a.contact());
-		System.out.println(a.perte_de_pv(pv_mon));	
-        pv_mon = a.perte_de_pv(pv_mon);
-		hero.setNb_PV(pv_mon);
-		System.out.println(a.perte_de_pv(pv_mon));
-		hero.setNb_PV(pv_mon);
-		System.out.println(hero);//marche pas
-		System.out.println(monstre);//marche pas
-		
+		//System.out.println(a.contact());
+		//a.attaque_contact(hero,monstre);
+		a.attaque_epe_contact(hero, monstre);
+		s.Potion(hero,potion);
+		s.Trou(hero,trou);
+		s.Pic(hero, pic);
+		a.attaque_epe(hero, monstre);
+		a.attaque_contact(hero, monstre);
+		System.out.println(hero);//marche
+		System.out.println(monstre);//marche
+		System.out.println(potion);
+		System.out.println(pic);
+		System.out.println(trou);
 	}
-
 }
