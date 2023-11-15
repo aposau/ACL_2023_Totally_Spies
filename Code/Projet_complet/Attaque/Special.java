6b45f48b1de6d292ef9579a9ae9a0dbbcfe6509d
romane Ransan <ransanrr@gmail.com>
2023-11-15 09:46:29 +0100

    attaque maj

--- Code/Projet_complet/Attaque/Special.java
+++ Code/Projet_complet/Attaque/Special.java
@@ -2,58 +2,73 @@ package proj_Ro;
 
 public class Special {
 
-		int X_pic;
-		int Y_pic;
-		
-		int X_trou;
-		int Y_trou;
+	int X_pic;
+	int Y_pic;
 
-		int X_hero;
-		int Y_hero;
-		
-		int X_potion;
-		int Y_potion;
+	int X_trou;
+	int Y_trou;
+
+	int X_hero;
+	int Y_hero;
+
+	int X_potion;
+	int Y_potion;
+
+	int X_fin;
+	int Y_fin;
 
 
-		public Special ( Personnage charles,Objet_spe potion,Objet_spe trou,Objet_spe pic) {
+	public Special ( Personnage charles,Objet_spe potion,Objet_spe trou,Objet_spe pic,Objet_spe fin) {
 
-			this.X_hero= charles.getPositionX();
-			this.Y_hero= charles.getPositionY();
-			
-			this.X_potion= potion.getPositionX();
-			this.Y_potion= potion.getPositionY();
-			
-			this.X_trou= trou.getPositionX();
-			this.Y_trou= trou.getPositionY();
-			
-			this.X_pic= pic.getPositionX();
-			this.Y_pic= pic.getPositionY();
+		this.X_hero= charles.getPositionX();
+		this.Y_hero= charles.getPositionY();
 
-		}
-		public void Potion(Personnage charles,Objet_spe popo) {
-			if (this.X_potion == this.X_hero) {
-				if (this.Y_potion == this.Y_hero) {
-					int pv_hero=charles.getNb_PV();
-					charles.setNb_PV(pv_hero+50);
-					popo.setPlein(0);
-				}
+		this.X_potion= potion.getPositionX();
+		this.Y_potion= potion.getPositionY();
+
+		this.X_trou= trou.getPositionX();
+		this.Y_trou= trou.getPositionY();
+
+		this.X_pic= pic.getPositionX();
+		this.Y_pic= pic.getPositionY();
+
+		this.X_fin= fin.getPositionX();
+		this.Y_fin= fin.getPositionY();
+
+	}
+	public void Potion(Personnage charles,Objet_spe popo) {
+		if (this.X_potion == this.X_hero) {
+			if (this.Y_potion == this.Y_hero) {
+				int pv_hero=charles.getNb_PV();
+				charles.setNb_PV(pv_hero+50);
+				popo.setPlein(0);
 			}
 		}
-		public void Pic(Personnage charles,Objet_spe pic) {
-			if (this.X_pic == this.X_hero) {
-				if (this.Y_pic == this.Y_hero) {
-					int pv_hero=charles.getNb_PV();
-					charles.setNb_PV(pv_hero-100);
-					pic.setPlein(0);
-				}
+	}
+	public void Pic(Personnage charles,Objet_spe pic) {
+		if (this.X_pic == this.X_hero) {
+			if (this.Y_pic == this.Y_hero) {
+				int pv_hero=charles.getNb_PV();
+				charles.setNb_PV(pv_hero-100);
+				pic.setPlein(0);
 			}
 		}
-		public void Trou(Personnage charles,Objet_spe trou) {
-			if (this.X_trou == this.X_hero) {
-				if (this.Y_trou == this.Y_hero) {
-					charles.setNb_PV(0);
-					trou.setPlein(0);
-				}
+	}
+	public void Trou(Personnage charles,Objet_spe trou) {
+		if (this.X_trou == this.X_hero) {
+			if (this.Y_trou == this.Y_hero) {
+				charles.setNb_PV(0);
+				trou.setPlein(0);
 			}
 		}
+	}
+	public int Fin(Personnage charles,Objet_spe fin) {
+		int p=0;
+		if (this.X_fin == this.X_hero) {
+			if (this.Y_fin == this.Y_hero) {
+				System.out.println("Vous avez gagnez !!");
+			}
+		}
+		return(p);
+	} 
 }
--- Code/Projet_complet/Attaque/main_Attaque.java
+++ Code/Projet_complet/Attaque/main_Attaque.java
@@ -1,4 +1,5 @@
 package proj_Ro;
+import javax.swing.*;
 
  
 public class main_Attaque {
@@ -8,14 +9,15 @@ public class main_Attaque {
 		int pv_mon=150;
 		int pv_hero=150;
 		int plein=1;
-		Personnage monstre= new Personnage(pv_mon,10,3);
-		Personnage hero= new Personnage(pv_hero,20,10);
+		Personnage monstre= new Personnage(pv_mon,10,3,9,2);
+		Personnage hero= new Personnage(pv_hero,20,10,19,9);
 		Objet_spe potion=new Objet_spe(plein,3,4);
 		Objet_spe trou=new Objet_spe(plein,15,4);
 		Objet_spe pic=new Objet_spe(plein,20,10);
+		Objet_spe fin=new Objet_spe(plein,20,10);
 
 		Attaque a=new Attaque(monstre, hero);
-		Special s=new Special(hero,potion,trou,pic);
+		Special s=new Special(hero,potion,trou,pic,fin);
 		
 		//System.out.println(a.contact());
 		//a.attaque_contact(hero,monstre);
@@ -30,5 +32,11 @@ public class main_Attaque {
 		System.out.println(potion);
 		System.out.println(pic);
 		System.out.println(trou);
+		
+		SwingUtilities.invokeLater(() -> {
+			int niveau=1;
+			LabyrintheGameGUI labyrinthe = new LabyrintheGameGUI(niveau);
+			labyrinthe.createAndShowGUI();
+	        });
 	}
 }
