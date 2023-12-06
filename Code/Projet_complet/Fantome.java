e912b26c7e114184262a736c8c605b01b4d68ee5
unknown <guillianlagauche@icloud.com>
2023-12-06 16:35:00 +0100

    src des monstres et fantomes updater

--- Code/Projet_complet/Fantome.java
+++ Code/Projet_complet/Fantome.java
@@ -1,8 +1,38 @@
 package projet_totally_spies;
 
+import java.util.Random;
+
 public class Fantome extends Monstre {
 	public Fantome(int nb_PV ,int positionX,int positionY, int ant_positionX, int ant_positionY,int matricule) {
 		super(nb_PV ,positionX,positionY,ant_positionX, ant_positionY, matricule);
 	}
-	
+	public void deplacement_random_mur() {
+		this.ant_positionX = this.positionX ;
+		this.ant_positionY = this.positionY ;
+		while ( this.nb_PV!=0 ) { // 
+			Random r = new Random();
+			int direction = r.nextInt(4);
+			if (direction==0) { //haut
+				if (this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16 ) {
+					this.move_up();
+				}
+			}
+			if (direction==1) { //bas
+				if (this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
+					this.move_down();
+				}
+			}
+			if (direction==2) { //gauche
+				if (this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
+					this.move_left();
+				}
+			}
+			if (direction==3) { //droite
+				if (this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
+					this.move_right();
+				}
+			}
+		}
+
+	}
 }
--- Code/Projet_complet/Monstre_normal.java
+++ Code/Projet_complet/Monstre_normal.java
@@ -12,22 +12,22 @@ public class Monstre_normal extends Monstre{
 			Random r = new Random();
 			int direction = r.nextInt(4);
 			if (direction==0) { //haut
-				if (this.tableau[this.positionX][this.positionY-1]!=1) {
+				if (this.tableau[this.positionX][this.positionY-1]!=1 && this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
 					this.move_up();
 				}
 			}
 			if (direction==1) { //bas
-				if (this.tableau[this.positionX][this.positionY+1]!=1) {
+				if (this.tableau[this.positionX][this.positionY+1]!=1 && this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
 					this.move_down();
 				}
 			}
 			if (direction==2) { //gauche
-				if (this.tableau[this.positionX-1][this.positionY]!=1) {
+				if (this.tableau[this.positionX-1][this.positionY]!=1 && this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16 ) {
 					this.move_left();
 				}
 			}
 			if (direction==3) { //droite
-				if (this.tableau[this.positionX+1][this.positionY]!=1) {
+				if (this.tableau[this.positionX+1][this.positionY]!=1 && this.positionX>0 && this.positionX<16 && this.positionY >0 && this.positionY<16) {
 					this.move_right();
 				}
 			}
